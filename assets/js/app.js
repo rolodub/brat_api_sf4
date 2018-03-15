f3d90b4387c12c0ca8fff41ba22a3a9d274ca708
rolandduboue <rolodub@gmail.com>
2018-03-15 02:41:19 +0100

    carte dans vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,18 @@ import VueRouter from 'vue-router';
 import Accueil from './components/Accueil.vue';
 import Rechercher from './components/Rechercher.vue';
 import Carte from './components/Carte.vue';
+import GoogleMap from './components/GoogleMap.vue';
 import Profil from './components/Profil.vue';
 import Contribuer from './components/Contribuer.vue';
+import * as VueGoogleMaps from "vue2-google-maps";
+
 Vue.use( VueRouter );
-
-
-// const Foo = { component: Example };
-const Bar = { template: '<div>bar</div>' };
+// Vue.use(VueGoogleMaps, {
+//     load: {
+//         key: "AIzaSyCkHwJPvvVaX0eC2S1XHKuwuDjePSS2uTs",
+//         libraries: "places" // necessary for places input
+//     }
+// });
 
 const routes = [
     { path: '/accueil', component: Accueil },
@@ -23,6 +28,8 @@ const router = new VueRouter({
     routes
 });
 
-const app = new Vue({
-    router
-}).$mount('#app');
+new Vue({
+    el: '#app',
+    router,
+
+});
--- assets/js/components/Carte.vue
+++ assets/js/components/Carte.vue
@@ -1,17 +1,47 @@
 <template>
-    <div type="x-template" id="carte">
-    <div>
-        <div id="map"></div>
-    </div>
-</div>
+    <div id="map"></div>
 </template>
 
 <script>
     export default {
-        name: "carte"
+        data: function () {
+            return {
+                mapName: this.name + "-map",
+                markerCoordinates: [{
+                    latitude: 42.501527,
+                    longitude: -0.1921837
+                }, {
+                    latitude: 43.505874,
+                    longitude: -0.1838486
+                }, {
+                    latitude: 45.4998973,
+                    longitude: -0.202432
+                }]
+            }
+        },
+        mounted: function() {
+            this.initMap();
+        },
+        methods: {
+            initMap: function() {
+                this.map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 48.862725, lng: 2.287592  },
+                    scrollwheel: false,
+                    zoom: 12
+                });
+                this.markerCoordinates.forEach((coord) => {
+                    const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+                    const marker = new google.maps.Marker({
+                        position,
+                        map: this.map
+                    });
+                    this.markers.push(marker);
+                    this.map.fitBounds(this.bounds.extend(position))
+                });
+            }
+        }
     }
 </script>
-
 <style scoped>
-
+    #map {height:300px;width:500px;}
 </style>
